a4db6f1d5497a7c785250ce02ed00f72b79adbe6
monaallam62 <monaallam5102@gmail.com>
2026-06-04 19:19:10 +0300

    PlanController

--- Gym_Project/Controllers/PlanController.cs
+++ Gym_Project/Controllers/PlanController.cs
@@ -1,28 +1,35 @@
 ﻿using Gym_Project.Contexts;
+using GymManagement.DAL.Repositories.Classes;
 using GymManagement.DAL.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Gym_Project.Controllers
-{
+{ 
     public class PlanController : Controller
     {
         // [1] Database Connection
         //private readonly GymDbContext context;
 
-        private readonly IPlanRepository planRepository;
+        private readonly IPlanRepository _planRepository;
+
+
+        public PlanController(IPlanRepository planRepository)
+        {
+            _planRepository = planRepository;
+        }
 
         //Get :: BaseUrl/Plan/Index.
         public async Task<IActionResult> Index(CancellationToken ct)
         {
-            var plans = await planRepository.GetAllAsync(ct:ct); //pass by name to avoid ambiguity with the cancellation token parameter
+            var plans = await _planRepository.GetAllAsync(ct:ct); //pass by name to avoid ambiguity with the cancellation token parameter
             return View(plans);
         }
         //GET :: BaseUrl/Plan/Details/{id}
         public async Task<IActionResult> Details(int id ,CancellationToken ct)
         {
-            var plan = await planRepository.GetByIdAsync(id, ct);
+            var plan = await _planRepository.GetByIdAsync(id, ct);
             if (plan == null)
                 return RedirectToAction(nameof(Index));
             return View(plan);
