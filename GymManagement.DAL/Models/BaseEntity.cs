0cb2ff59ba3b25dc732e1456f71d56265420b6b6
monaallam62 <monaallam5102@gmail.com>
2026-06-04 22:30:57 +0300

    Models

--- GymManagement.DAL/Models/BaseEntity.cs
+++ GymManagement.DAL/Models/BaseEntity.cs
@@ -8,6 +8,7 @@ namespace GymManagement.DAL.Models
 {
     public class BaseEntity
     {
+        public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
