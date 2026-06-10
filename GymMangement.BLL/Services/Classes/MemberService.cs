58932a58816859bd30f53789e398df96a0cff584
monaallam62 <monaallam5102@gmail.com>
2026-06-10 22:55:46 +0300

    GemManagement

--- GymMangement.BLL/Services/Classes/MemberService.cs
+++ GymMangement.BLL/Services/Classes/MemberService.cs
@@ -1,6 +1,7 @@
 ﻿using GymManagement.DAL.Models;
 using GymManagement.DAL.Repositories.Interfaces;
 using GymMangement.BLL.Services.Interfaces;
+using GymMangement.BLL.ViewModels;
 using GymMangement.BLL.ViewModels.MemberViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,41 @@ namespace GymMangement.BLL.Services.Classes
         {
             _memberRepo = memberRepo;
         }
+
+        public async Task<bool> CreateMemberAsync(CreateMemberViewModel model, CancellationToken ct = default)
+        {
+            //Email Exist or Not
+            var EmailExist =await _memberRepo.AnyAsync(X => X.Email == model.Email);
+            //Phone Exist Or Not
+            var PhoneExist = await _memberRepo.AnyAsync(X => X.Phone == model.Phone);
+
+            if (EmailExist || PhoneExist) return false;
+            //Add Member
+            var member = new Member()
+            {
+                Name =model.Name,
+                Email = model.Email,
+                Phone = model.Phone,
+                Gender = model.Gender,
+                DateOfBirth = model.DateOfBirth,
+                Address = new Address
+                {
+                    BuildingNumber = model.BuildingNumber,
+                    City = model.City,
+                    Street = model.Street
+                },
+                HealthRecord =new HealthRecord()
+                {
+                    BloodType =model.HealthRecordViewModel.BloodType,
+                    Height = model.HealthRecordViewModel.Height,
+                    Weight = model.HealthRecordViewModel.Weight,
+                    Note =model.HealthRecordViewModel.Note
+                }
+            };
+            var result = await _memberRepo.AddAsync(member);
+            return result > 0;
+        }
+
         public async Task<IEnumerable<MemberViewModel>> GetAllAsync(CancellationToken ct = default)
         {
 
--- GymMangement.BLL/Services/Interfaces/IMemberService.cs
+++ GymMangement.BLL/Services/Interfaces/IMemberService.cs
@@ -1,4 +1,5 @@
 ﻿using GymManagement.DAL.Models;
+using GymMangement.BLL.ViewModels;
 using GymMangement.BLL.ViewModels.MemberViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,9 @@ namespace GymMangement.BLL.Services.Interfaces
 {
     public interface IMemberService
     {
+        //Gett All
         Task<IEnumerable<MemberViewModel>> GetAllAsync(CancellationToken ct = default);
+        //Create Member
+        Task<bool> CreateMemberAsync(CreateMemberViewModel member, CancellationToken ct = default);
     }
 }
