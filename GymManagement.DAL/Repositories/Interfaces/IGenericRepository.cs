b342ade65ef1661b5952cce161b83b8d6cddc3d9
monaallam62 <monaallam5102@gmail.com>
2026-06-10 19:42:29 +0300

    Gymmanagement

---
+++ GymManagement.DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using GymManagement.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity :BaseEntity , new()
+    {
+        //GetById
+        Task<TEntity?> GetByIdAsync(int id, CancellationToken ct=default);
+        //Add
+        Task<int> AddAsync(TEntity entity);
+        //Update
+        Task<int> UpdateAsync(TEntity entity);
+        //Delete
+        Task<int> DeleteAsync(TEntity entity);
+        //GetAll
+        Task<IEnumerable<TEntity>> GetAllAsync(bool tracking=false ,CancellationToken ct=default);
+    }
+}
