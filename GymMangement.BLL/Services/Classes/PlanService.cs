4f3cdbd54e6294d38c55cdc173df99914581e385
monaallam62 <monaallam5102@gmail.com>
2026-06-13 16:20:22 +0300

    Gym_Projects

---
+++ GymMangement.BLL/Services/Classes/PlanService.cs
@@ -0,0 +1,101 @@
+﻿using Gym_Project.Models;
+using GymManagement.DAL.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using GymMangement.BLL.Services.Interfaces;
+using GymMangement.BLL.ViewModels.PlanViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangement.BLL.Services.Classes
+{
+    public class PlanService : IPlanService
+    {
+        //DataConnection 
+        private readonly IGenericRepository<Plan> _planRepository;
+        private readonly IGenericRepository<Membership> _membershipRepository;
+
+        public PlanService(IGenericRepository<Plan> planRepository, IGenericRepository<Membership> membershipRepository)
+        {
+            _planRepository = planRepository;
+            _membershipRepository = membershipRepository;
+        }
+
+        public async Task<IEnumerable<PlanViewModel>> GetAllPlansAsync(CancellationToken ct = default)
+        {
+            var plans = await _planRepository.GetAllAsync(ct: ct);
+            return plans.Select(p => new PlanViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                DurationDays = p.DurationDays,
+                Description = p.Description,
+                IsActive = p.IsActive
+            });
+        }
+
+        public async Task<PlanViewModel?> GetPlanByIdAsync(int planid, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planid, ct);
+            if (plan is null) return null;
+            else
+                return new PlanViewModel
+                {
+                    Name = plan.Name,
+                    Price = plan.Price,
+                    DurationDays = plan.DurationDays,
+                    Description = plan.Description,
+                    IsActive = plan.IsActive
+                };
+        }
+
+        public async Task<UpdatePlanViewModel> GetPlanToUpdateAsync(int planId, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan is null || !plan.IsActive) return null;
+            if (await HasActiveMembershipsAsync(planId, ct))
+                return null;
+            else
+                return new UpdatePlanViewModel
+                {
+                    PlanName = plan.Name,
+                    Price = plan.Price,
+                    DurationDays = plan.DurationDays,
+                    Description = plan.Description
+                };
+        }
+
+        public async Task<bool> ToggleActivationAsync(int planId, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan is null) return false;
+            if (plan.IsActive && await HasActiveMembershipsAsync(planId, ct))
+                return false;
+        }
+
+        public async Task<bool> UpdatePlanAsync(int id, UpdatePlanViewModel model, CancellationToken ct = default)
+        {
+            var plan =await _planRepository.GetByIdAsync(id, ct);
+            if(plan is null) return false;
+            if(await HasActiveMembershipsAsync(id,ct))
+                return false;
+
+            plan.DurationDays = model.DurationDays;
+            plan.Description = model.Description;
+            plan.Price = model.Price;
+            plan.UpdatedAt = DateTime.Now; 
+             
+            var result = await _planRepository.UpdateAsync(plan, ct);
+            return result > 0;
+        }
+
+        private async Task<bool> HasActiveMembershipsAsync(int planId, CancellationToken ct)
+        {
+            return await _membershipRepository.AnyAsync(m => m.PlanId == planId && m.EndDate > DateTime.Now, ct);
+
+        }
+    }
+}
--- Gym_Project/Controllers/PlanController.cs
+++ Gym_Project/Controllers/PlanController.cs
@@ -2,37 +2,53 @@
 using Gym_Project.Models;
 using GymManagement.DAL.Repositories.Classes;
 using GymManagement.DAL.Repositories.Interfaces;
+using GymMangement.BLL.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Gym_Project.Controllers
-{ 
+{
     public class PlanController : Controller
     {
         // [1] Database Connection
         //private readonly GymDbContext context;
+        private readonly IPlanService _planService;
 
-        private readonly IGenericRepository<Plan> _planRepository;
+        //private readonly IGenericRepository<Plan> _planRepository;
 
 
-        public PlanController(IGenericRepository<Plan> planRepository)
+        public PlanController(IGenericRepository<Plan> planRepository, IPlanService planService)
         {
-            _planRepository = planRepository;
+            //_planRepository = planRepository;
+            _planService = planService;
         }
 
         //Get :: BaseUrl/Plan/Index.
         public async Task<IActionResult> Index(CancellationToken ct)
-        {
-            var plans = await _planRepository.GetAllAsync(ct:ct); //pass by name to avoid ambiguity with the cancellation token parameter
-            return View(plans);
-        }
+            => View(await _planService.GetAllPlansAsync(ct));
+        //public async Task<IActionResult> Index(CancellationToken ct)
+        //{
+        //    var plans = await _planRepository.GetAllAsync(ct:ct); //pass by name to avoid ambiguity with the cancellation token parameter
+        //    return View(plans);
+        //}
         //GET :: BaseUrl/Plan/Details/{id}
-        public async Task<IActionResult> Details(int id ,CancellationToken ct)
+        //public async Task<IActionResult> Details(int id ,CancellationToken ct)
+        //{
+        //    var plan = await _planRepository.GetByIdAsync(id, ct);
+        //    if (plan == null)
+        //        return RedirectToAction(nameof(Index));
+        //    return View(plan);
+        //}
+        [HttpGet]
+        public async Task<IActionResult> Details(int id, CancellationToken ct)
         {
-            var plan = await _planRepository.GetByIdAsync(id, ct);
+            var plan = await _planService.GetPlanByIdAsync(id, ct);
             if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan not found.";
                 return RedirectToAction(nameof(Index));
+            }
             return View(plan);
         }
     }
