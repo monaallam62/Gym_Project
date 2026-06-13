d8a0e4f07bb6092e153da3ddbbba2c242a071c0e
monaallam62 <monaallam5102@gmail.com>
2026-06-13 17:21:56 +0300

    Projects

--- GymMangement.BLL/ViewModels/PlanViewModels/PlanViewModel.cs
+++ GymMangement.BLL/ViewModels/PlanViewModels/PlanViewModel.cs
@@ -9,10 +9,10 @@ namespace GymMangement.BLL.ViewModels.PlanViewModels
     public class PlanViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; } = default!;
         public decimal Price { get; set; }
         public int DurationDays { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = default!;
         public bool IsActive { get; set; }
     }
 }
